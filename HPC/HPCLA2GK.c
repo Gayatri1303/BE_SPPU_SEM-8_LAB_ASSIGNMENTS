8ce41a9d74d35f6dfa1038e2deb402c3763d5edb
Gayatri1303 <89266589+Gayatri1303@users.noreply.github.com>
2025-04-26 10:00:39 +0530

    Update HPCLA2GK.c

--- HPC/HPCLA2GK.c
+++ HPC/HPCLA2GK.c
@@ -3,6 +3,8 @@
 #include <omp.h>
 #include <time.h>
 
+#define THRESHOLD 1000
+
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
@@ -21,34 +23,17 @@ void merge(int arr[], int left, int mid, int right) {
     int *L = (int *)malloc(n1 * sizeof(int));
     int *R = (int *)malloc(n2 * sizeof(int));
 
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
 
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
     }
 
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
 
     free(L);
     free(R);
@@ -67,52 +52,73 @@ void mergeSortParallel(int arr[], int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
 
-        #pragma omp parallel sections
-        {
-            #pragma omp section
-            mergeSortParallel(arr, left, mid);
-            
-            #pragma omp section
-            mergeSortParallel(arr, mid + 1, right);
-        }
+        if ((right - left) <= THRESHOLD) {
+            mergeSortSequential(arr, left, mid);
+            mergeSortSequential(arr, mid + 1, right);
+        } else {
+            #pragma omp parallel sections
+            {
+                #pragma omp section
+                mergeSortParallel(arr, left, mid);
 
+                #pragma omp section
+                mergeSortParallel(arr, mid + 1, right);
+            }
+        }
         merge(arr, left, mid, right);
     }
 }
 
 void bubbleSortOddEvenParallel(int arr[], int n) {
-    int temp;
+    int isSorted = 0;
+    while (!isSorted) {
+        isSorted = 1;
 
-    // Parallelize Odd Phase
-    #pragma omp parallel for private(temp)
-    for (int i = 0; i < n / 2; i++) {
-        for (int j = 1 + i; j < n - i - 1; j += 2) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+        #pragma omp parallel for
+        for (int i = 1; i < n - 1; i += 2) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                isSorted = 0;
             }
         }
-    }
 
-    // Parallelize Even Phase
-    #pragma omp parallel for private(temp)
-    for (int i = 0; i < n / 2; i++) {
-        for (int j = i; j < n - i - 1; j += 2) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+        #pragma omp parallel for
+        for (int i = 0; i < n - 1; i += 2) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                isSorted = 0;
             }
         }
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+void bubbleSortOddEvenSequential(int arr[], int n) {
+    int isSorted = 0;
+    while (!isSorted) {
+        isSorted = 1;
+
+        for (int i = 1; i < n - 1; i += 2) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                isSorted = 0;
+            }
+        }
+
+        for (int i = 0; i < n - 1; i += 2) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                isSorted = 0;
+            }
+        }
     }
-    printf("\n");
 }
 
 void generateRandomArray(int arr[], int n) {
@@ -123,6 +129,7 @@ void generateRandomArray(int arr[], int n) {
 
 int main() {
     srand(time(0));
+    omp_set_num_threads(omp_get_max_threads());  // Use all available threads
 
     int n;
     printf("Enter the size of the array: ");
@@ -131,69 +138,63 @@ int main() {
     int *arr1 = (int *)malloc(n * sizeof(int));
     int *arr2 = (int *)malloc(n * sizeof(int));
     int *arr3 = (int *)malloc(n * sizeof(int));
+    int *arr4 = (int *)malloc(n * sizeof(int));
 
-    // Generate random numbers for the array
-    generateRandomArray(arr1, n);
-    for (int i = 0; i < n; i++) {
-        arr2[i] = arr1[i];
-        arr3[i] = arr1[i];
-    }
-    printf("Gayatri KUrulkar 41039 BE A");
-    printf("\n+----------------------+------------------------+------------------------+------------------+------------------+\n");
-    printf("| Iteration | Odd-Even Bubble Sort Seq Time | Odd-Even Bubble Sort Par Time | Merge Sort Seq Time | Merge Sort Par Time | Speedup | Efficiency |\n");
-    printf("+----------------------+------------------------+------------------------+------------------+------------------+\n");
+    printf("\nGayatri Kurulkar 41039 BE A\n");
+    printf("+-----------+------------------------------+------------------------------+----------------------+----------------------+------------+-------------+\n");
+    printf("| Iteration | Odd-Even Bubble Sort Seq Time | Odd-Even Bubble Sort Par Time | Merge Sort Seq Time | Merge Sort Par Time | Speedup    | Efficiency  |\n");
+    printf("+-----------+------------------------------+------------------------------+----------------------+----------------------+------------+-------------+\n");
 
-    // Measure performance of sequential algorithms and parallel algorithms for 5 observations
     for (int i = 0; i < 5; i++) {
-        // Sequential Odd-Even Bubble Sort
-        double start_time = omp_get_wtime();
-        bubbleSortOddEvenParallel(arr1, n);
-        double end_time = omp_get_wtime();
-        double bubbleSortSeqTime = end_time - start_time;
-
-        // Parallel Odd-Even Bubble Sort
-        start_time = omp_get_wtime();
-        bubbleSortOddEvenParallel(arr1, n);
-        end_time = omp_get_wtime();
-        double bubbleSortParTime = end_time - start_time;
-
-        // Sequential Merge Sort or Insertion Sort based on size
-        start_time = omp_get_wtime();
-        if (n < 20) {
-            insertionSort(arr2, n);  // Use Insertion Sort if n is less than 20
-        } else {
-            mergeSortSequential(arr2, 0, n - 1);  // Otherwise, use Merge Sort
+        generateRandomArray(arr1, n);
+        for (int j = 0; j < n; j++) {
+            arr2[j] = arr1[j];
+            arr3[j] = arr1[j];
+            arr4[j] = arr1[j];
         }
-        end_time = omp_get_wtime();
-        double mergeSortSeqTime = end_time - start_time;
 
-        // Parallel Merge Sort or Insertion Sort based on size
+        double start_time, end_time;
+
+        // Odd-Even Bubble Sort Sequential
         start_time = omp_get_wtime();
-        if (n < 20) {
-            insertionSort(arr3, n);  // Use Insertion Sort if n is less than 20
-        } else {
-            mergeSortParallel(arr3, 0, n - 1);  // Otherwise, use Parallel Merge Sort
-        }
+        bubbleSortOddEvenSequential(arr1, n);
         end_time = omp_get_wtime();
-        double mergeSortParTime = end_time - start_time;
+        double bubbleSeqTime = end_time - start_time;
 
-        // Calculate Speedup and Efficiency
-        double bubbleSortSpeedup = bubbleSortSeqTime / bubbleSortParTime;
-        double mergeSortSpeedup = mergeSortSeqTime / mergeSortParTime;
-        double efficiency = (bubbleSortSpeedup + mergeSortSpeedup) / 6;
+        // Odd-Even Bubble Sort Parallel
+        start_time = omp_get_wtime();
+        bubbleSortOddEvenParallel(arr2, n);
+        end_time = omp_get_wtime();
+        double bubbleParTime = end_time - start_time;
 
-        // Print results in table format
-        printf("| %9d | %22f | %22f | %16f | %16f | %8f | %10f |\n", 
-               i + 1, bubbleSortSeqTime, bubbleSortParTime, mergeSortSeqTime, mergeSortParTime, 
-               bubbleSortSpeedup + mergeSortSpeedup, efficiency);
+        // Merge Sort Sequential or Insertion
+        start_time = omp_get_wtime();
+        if (n < 20) insertionSort(arr3, n);
+        else mergeSortSequential(arr3, 0, n - 1);
+        end_time = omp_get_wtime();
+        double mergeSeqTime = end_time - start_time;
+
+        // Merge Sort Parallel or Insertion
+        start_time = omp_get_wtime();
+        if (n < 20) insertionSort(arr4, n);
+        else mergeSortParallel(arr4, 0, n - 1);
+        end_time = omp_get_wtime();
+        double mergeParTime = end_time - start_time;
+
+        double bubbleSpeedup = bubbleSeqTime / bubbleParTime;
+        double mergeSpeedup = mergeSeqTime / mergeParTime;
+        double efficiency = (bubbleSpeedup + mergeSpeedup) / (2 * omp_get_max_threads());
+
+        printf("| %9d | %28f | %28f | %20f | %20f | %10f | %11f |\n",
+               i + 1, bubbleSeqTime, bubbleParTime, mergeSeqTime, mergeParTime,
+               bubbleSpeedup + mergeSpeedup, efficiency);
     }
 
-    printf("+----------------------+------------------------+------------------------+------------------+------------------+\n");
+    printf("+-----------+------------------------------+------------------------------+----------------------+----------------------+------------+-------------+\n");
 
     free(arr1);
     free(arr2);
     free(arr3);
-
+    free(arr4);
     return 0;
 }
-
